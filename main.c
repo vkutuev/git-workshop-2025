347a22450162fffb15b2566887c35250336f4755
Vladimir Kutuev <vladimir.kutuev@gmail.com>
2025-09-08 14:30:47 +0300

    Add factorial function

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+int fact(int);
+
 int main() {
 	printf("Hello Vova!\n");
+	printf("fact(5) == %d\n", fact(5));
 	printf("Goodbye Vova!\n");
 	return 0;
+}
+
+int fact(int n) {
+	int acc = 1;
+	for (; n > 0; acc *= n--);
+	return acc;
 }
